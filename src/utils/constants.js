7586632f5b3fd1334bbb745f3a49656a1e0b03f5
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-07-15 05:16:05 +0530

    mock api urls added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,10 @@ export const LOGO_URL = 'https://graphicsfamily.com/wp-content/uploads/edd/2021/
 
 export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
 
-export const RESTAURANT_DATA_API = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1458004&lng=79.0881546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+export const RESTAURANT_DATA_API = 'https://food-app.deepanshu-sahu-projects.live/api/restaurant'
 
-export const RESTAURANT_MENU_API = 'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1458004&lng=79.0881546&restaurantId='
+export const RESTAURANT_MENU_API = 'https://food-app.deepanshu-sahu-projects.live/api/restaurant/'
+
+export const RESTAURANT_UPDATE_API = 'https://food-app.deepanshu-sahu-projects.live/api/update'
 
 export const GITHUB_USER_API = 'https://api.github.com/users/dee077'
