91c36634c057a5b3b7a1d2ff20d441366fd7adca
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-08-06 02:39:28 +0530

    deploy script

--- deploy-fe.sh
+++ deploy-fe.sh
@@ -5,6 +5,24 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Pull the latest changes from GitHub
+git pull 
+if [ $? -eq 0 ]; then
+    echo -e "\n${GREEN}Git pull successful${NC}\n"
+else
+    echo -e "\n${RED}Git pull failed${NC}\n"
+    exit 1
+fi
+
+# Install npm dependencies
+npm install
+if [ $? -eq 0 ]; then
+    echo -e "\n${GREEN}npm install successful${NC}\n"
+else
+    echo -e "\n${RED}npm install failed${NC}\n"
+    exit 1
+fi
+
 # Run npm build
 npm run build
 if [ $? -eq 0 ]; then
