51693715ac69c963f37e9b5205bb646cb65082ac
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-09-20 20:51:43 +0530

    domain changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,11 +2,11 @@ export const LOGO_URL = 'https://graphicsfamily.com/wp-content/uploads/edd/2021/
 
 export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
 
-export const RESTAURANT_DATA_API = 'https://food-app.deepanshu-sahu-projects.live/api/restaurant'
+export const RESTAURANT_DATA_API = 'https://food-app-be-gamma.vercel.app/api/restaurant'
 
-export const RESTAURANT_MENU_API = 'https://food-app.deepanshu-sahu-projects.live/api/restaurant/'
+export const RESTAURANT_MENU_API = 'https://food-app-be-gamma.vercel.app/api/restaurant/'
 
-export const RESTAURANT_UPDATE_API = 'https://food-app.deepanshu-sahu-projects.live/api/update'
+export const RESTAURANT_UPDATE_API = 'https://food-app-be-gamma.vercel.app/api/update'
 
 export const GITHUB_USER_API = 'https://api.github.com/users/dee077'
 
